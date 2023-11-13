3f9bb1bb996723fd6ec359aa0b07928c0c44dc15
OG'ABEK <o.shomurotov@uic.group>
2023-11-13 21:28:14 +0500

    feat: login form added

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    Login
+  <div class='container pt-5'>
+    <div class='w-50 mx-auto bg-white shadow p-4 border mt-5 rounded-3'>
+      <h3>Login</h3>
+      <form>
+        <div class='mb-3'>
+          <label for='exampleInputEmail1' class='form-label'>Email address</label>
+          <input type='email' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp'>
+        </div>
+        <div class='mb-3'>
+          <label for='exampleInputPassword1' class='form-label'>Password</label>
+          <input type='password' class='form-control' id='exampleInputPassword1'>
+        </div>
+        <button type='submit' class='btn btn-primary w-100'>Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
